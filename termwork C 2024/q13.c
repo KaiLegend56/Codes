96a950156e758e7575ddab485ba9a73b81cbfbf0
KaiLegend56 <kaininja567@gmail.com>
2024-05-10 21:49:57 +0530

    q13 termwork

--- termwork C 2024/q13.c
+++ termwork C 2024/q13.c
@@ -5,13 +5,25 @@ int main(){
     FILE *q=fopen("Count.txt","w+");
     char a[100];
     int i;
-    printf("%d",fgets(a,100,stdin));
     while(fgets(a,100,stdin)!=NULL){
        
        fputs(a,p);
 
 
     }
+    char ch;
+    int alphabetCount=0,numCount=0;
     rewind(p);
-
+    while(!feof(p)){
+      ch=fgetc(p);
+      if(ch>='a'&&ch<='z')
+      alphabetCount++;
+          
+      if(ch>='0'&&ch<='9')    
+      numCount++;
+      if(ch=='\n'){
+      fprintf(q,"%d %d \n",alphabetCount,numCount);
+      numCount=0,alphabetCount=0;
+      }
+}
     }
